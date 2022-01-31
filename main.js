f864461c4710c10b05a831aef8808f01fd627bdb
suchitchauhan <87800855+suchitchauhan@users.noreply.github.com>
2022-01-31 15:40:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,74 @@ var input = document.querySelector('.todo_input');
 var addButton = document.querySelector('.add-item');
 var deleteAllButton = document.querySelector('.deleteBtn');
 
+const count = 0;
+
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the d
+        if (input.value.trim()) {
+
+            var ultag = document.createElement('ul');
+            ultag.classList.add('todo-list-container');
+
+
+            //div 
+            var todolist = document.createElement('div');
+            todolist.classList.add('todo-list');
+
+
+            var litag = document.createElement('li');
+            litag.innerHTML = input.value;
+            litag.classList.add('todo-item');
+
+            //button
+            var buttonDiv = document.createElement('div');
+            buttonDiv.classList.add('button');
+
+            var completeButton = document.createElement('INPUT');
+            completeButton.classList.add('completed');
+            completeButton.setAttribute("type", "checkbox");
+            completeButton.setAttribute("onclick", "complete_mark(this)");
+
+            var editButton = document.createElement('button');
+            editButton.classList.add('editBtn');
+            editButton.innerHTML = '<abbr title="EditForClick"><i class="fas fa-edit" ></i></abbr>';
+
+            editButton.onclick = function() {
+                edit_working(litag);
+            }
+
+            var delteButton = document.createElement('button');
+            delteButton.classList.add('trash');
+            delteButton.innerHTML = '<abbr title="DeleteForClick"><i class="fas fa-trash" ></i></abbr>';
+            delteButton.onclick = function() {
+                delete_working(ultag);
+            }
+
+
+            ultag.appendChild(todolist);
+            todolist.appendChild(litag);
+            todolist.appendChild(buttonDiv);
+
+            buttonDiv.appendChild(completeButton);
+            buttonDiv.appendChild(editButton);
+            buttonDiv.appendChild(delteButton);
+
+            MainTodoContainer.appendChild(ultag);
+
+
+            input.value = "";
+
+            deleteBtn.style.display = "block";
+            count++;
+
+        }
+        // Trigger the button element with a click
+    }
+});
+
+
 addButton.addEventListener("click", function(e) {
     if (input.value.trim()) {
 
@@ -25,9 +93,9 @@ addButton.addEventListener("click", function(e) {
 
         var completeButton = document.createElement('button');
         completeButton.classList.add('completed');
-        completeButton.innerHTML = '<abbr title="CheckMark"><i class="fas fa-check" ></i></abbr>';
+        completeButton.innerHTML = '<input type="checkbox" name="check_marks_data" id="chkEmail"></input>';
         completeButton.onclick = function() {
-            complete_mark(ultag);
+            complete_mark(this);
         }
 
         var editButton = document.createElement('button');
@@ -58,6 +126,8 @@ addButton.addEventListener("click", function(e) {
 
 
         input.value = "";
+        deleteBtn.style.display = "block";
+        count++;
 
 
     } else if (input.value === '') {
@@ -77,10 +147,36 @@ function edit_working(e) {
     } else {
         return false;
     }
+}
 
+function delete_working(e) {
+
+    var msg = "Delete Confirmation";
+    var deletecon = confirm(msg);
+    if (deletecon) {
+        var getting_Ultag1 = document.querySelectorAll('.todo-list-container');
+
+        var length_count = getting_Ultag1.length;
+        for (var i = 0; i < getting_Ultag1.length; i++) {
+            {
+                e.remove();
+                //alert(getting_Ultag1.length);
+                if (length_count == 1) {
+                    deleteBtn.style.display = "none";
+                } else {
+                    deleteBtn.style.display = "block";
+                }
+            }
+        }
+
+
+    } else {
+        return false;
+    }
 
 
 }
+
 
 function deleteAllTask() {
 
@@ -92,6 +188,7 @@ function deleteAllTask() {
             {
                 getting_Ultag[i].remove();
             }
+            deleteBtn.style.display = "none";
         }
 
     } else {
@@ -100,24 +197,23 @@ function deleteAllTask() {
 
 }
 
-function delete_working(e) {
-    var msg = "Delete Confirmation";
-    var deletecon = confirm(msg);
-    if (deletecon) {
 
-        return e.remove();
-    } else {
-        return false;
-    }
-}
+
 
 function complete_mark(e) {
-    var msg1 = "Check the Task";
-    var checktask = confirm(msg1);
-    if (checktask) {
-        return e.classList.add('line_through');
+    if (e.checked) {
+        var item = e.parentNode.parentNode.firstChild;
+        item.id = "newText";
+        swal({
+            title: "Task",
+            text: "Task is completed",
+            icon: "success",
+            button: "Submit",
+        });
     } else {
-        return false;
+
+        var item = e.parentNode.parentNode.firstChild;
+        item.removeAttribute("id");
     }
 
 }
